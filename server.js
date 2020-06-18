064a5a690a7f347c0f53a6a99466cdecb3f89c0a
David Stewart <darkorin00@hotmail.com>
2020-06-17 22:51:32 -0400

    debugging

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/api/notes", function (req, res) {
 });
 app.delete("/api/notes/:id", function (req, res) {
     console.log(id);
-    res.sendFile(path.join(__dirname, "view.html"));
+    return id;
 });
 
 // Starts the server to begin listening
